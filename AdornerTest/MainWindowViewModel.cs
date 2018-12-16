370689e12e2b32477ad8b949c6b8f7d144f22816
TedMrucz <45922103+TedMrucz@users.noreply.github.com>
2018-12-16 16:17:36 -0500

    Add files via upload

---
+++ AdornerTest/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace AdornerTest
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string legendItem;
+        public string LegendItem
+        {
+            get { return legendItem; }
+            set { SetProperty(ref legendItem, value); }
+        }
+
+        private ObservableCollection<string> legendItems = new ObservableCollection<string>();
+        public ObservableCollection<string> LegendItems
+        {
+            get { return legendItems; }
+            set { SetProperty(ref legendItems, value); }
+        }
+
+        public DelegateCommand AddCommand { get; set; }
+        private void OnAddCommand()
+        {
+            this.LegendItems.Add(this.LegendItem);
+        }
+
+        public MainWindowViewModel()
+        {
+            this.AddCommand = new DelegateCommand(OnAddCommand);
+        }
+    }
+}
